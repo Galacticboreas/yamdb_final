4d174462ebc940c396b3ea07086214f33c35003c
Galacticboreas <ivanovvolgdonbor@mail.ru>
2021-05-23 18:31:22 +0300

    test_failed_import2

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,6 @@ from rest_framework.routers import SimpleRouter
 
 from .views import PostViewSet, UserViewSet
 
-
 router = SimpleRouter()
 router.register('users', UserViewSet, basename='users')
 router.register('', PostViewSet, basename='posts')
