61feacc1309a9a7f3adb46263181ef46ca706ec8
Galacticboreas <ivanovvolgdonbor@mail.ru>
2021-05-26 21:40:44 +0300

    Debag - True

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = int(os.environ.get("DEBUG", default=0))
-DEBUG = False
+DEBUG = True
 
 
 # ALLOWED_HOSTS = ['*']
